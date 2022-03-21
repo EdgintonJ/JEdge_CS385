56f0f9a9e0eb8cfaa2fffe1db3eac2d8873dbcf4
EdgintonJ <90742881+EdgintonJ@users.noreply.github.com>
2022-03-21 16:22:02 -0700

    Update main.js

--- assignment_3/main.js
+++ assignment_3/main.js
@@ -2,40 +2,42 @@
 // CS-385-02 Sp '22
 // Assignment 3
 
+
 let angle = 0;
+let gl;
+let canvas;
 
 function init(){
-    const canvas = document.getElementById("webgl-canvas");
-    const gl = canvas.getContext("webgl2");
+    canvas = document.getElementById("webgl-canvas");
+    gl = canvas.getContext("webgl2");
     if (gl === null) {
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
         return;
     }
 
-    /*let frontColor = vec4(0, 0xbb, 0xff, 1);
-    let backColor = vec4(1, 1, 1, 0);
-    let fColor = gl_FrontFacing ?
-        frontColor : backColor;*/
-
     gl.clearColor(0, 0xbb, 0xff, 1);
     let cube = new Cube(gl);
     gl.clearDepth(1.0); // default
     gl.enable(gl.DEPTH_TEST);
-    render(gl, cube);
+    setTimeout(() => {
+        requestAnimationFrame(render(cube));
+    }, 1000 / 10);
 
-
-    // render(gl, cube);
 }
 
 
-function render(gl, obj){
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+function render(obj){
 
     angle += 5;
+    obj.MV = translate(0, 0, 0)
     obj.MV = rotate(angle, [1, 1, 1]);  // rotate around the axis (1, 1, 1)
-    // obj.MV = scalem(0.95, 1.05, 1)
+    obj.P = lookAt(vec3(0, 0, -0.5), vec3(0, 0, 0), vec3(0, 1, 0))
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
     obj.render()
-    requestAnimationFrame(render)
+    setTimeout(() => {
+        requestAnimationFrame(render(obj));
+    }, 1000 / 10);
 
 }
 
